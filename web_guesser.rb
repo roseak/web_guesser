15b6420abe4f278f35068bccaf02983be10cf5a7
Rose Kohn <rose.kohn@gmail.com>
2015-08-16 21:07:54 -0600

    I4: colorizing the output complete.

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,20 +5,20 @@ SECRET_NUMBER = rand(100)
 
 def check_guess(guess)
   if guess > (SECRET_NUMBER + 5)
-    "Way too high!"
+    ["Way too high!", "red"]
   elsif guess > SECRET_NUMBER
-    "Too high!"
+    ["Too high!", "#FF6666"]
   elsif guess < (SECRET_NUMBER - 5)
-    "Way too low!"
+    ["Way too low!", "red"]
   elsif guess < SECRET_NUMBER
-    "Too low!"
+    ["Too low!", "#FF6666"]
   else
-    "The SECRET NUMBER is #{SECRET_NUMBER}"
+    ["The SECRET NUMBER is #{SECRET_NUMBER}", "green"]
   end
 end
 
 get '/' do
   guess = params["guess"].to_i
-  message = check_guess(guess)
-  erb :index, :locals => {:message => message}
+  message, color = check_guess(guess)
+  erb :index, :locals => {:color => color, :message => message}
 end
